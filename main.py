4143940c74566eaf2b4ca4325a660a9613398662
David Bakalarczyk <david.bakalarczyk00@gmail.com>
2024-08-19 19:19:30 +0200

    Improve mouse tracking, disable option to resize GUI by user

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from turtle import Turtle, Screen
 
 s = Screen()
 s.setup(1000, 600)
+s.tracer(0)
+s.cv._rootwindow.resizable(False, False)
 
 
 
@@ -16,19 +18,23 @@ t.setx(0)
 t.sety(-250)
 t.showturtle()
 
-def dragging(x, y):
-    t.ondrag(None)
-    t.setheading(0)
-    t.goto(x, -250)
-    t.ondrag(dragging)
+
+def follow_mouse():
+    x, y = s._root.winfo_pointerx() - s.window_width() // 1.29, s._root.winfo_pointery() - s.window_height() // 1.4
+    if -470 < x < 470:
+        t.setx(x)
+    s.update()
+    s.ontimer(follow_mouse, 10)
 
 def main():
-    turtle.listen()
-    t.ondrag(dragging)
+    follow_mouse()
     s.mainloop()
-
 
 main()
 
 
-
+# TODO 1: Paddle should follow the mouse pointer
+# TODO 2: Make the paddle borders on Screen border
+# TODO 3: Create the ball
+# TODO 4: Adjustment basic ball movement
+# TODO 5: Make contact and change moving direction
